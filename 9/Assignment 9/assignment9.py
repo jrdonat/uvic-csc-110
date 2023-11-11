55c53a6ae94a2388565635e351fdd5970feaa4be
DrydenD0Texe <drydenbryson@gmail.com>
2023-11-10 16:43:07 -0800

    Assugnment 9 starter files

---
+++ 9/Assignment 9/assignment9.py
@@ -0,0 +1,232 @@
+import doctest
+
+# all 2 digit years assumed to be in the 2000s
+START_YEAR = 2000
+
+# represents a Gregorian date as (year, month, day)
+#  where year>=START_YEAR, 
+#  month is a valid month, 1-12 to represent January-December
+#  and day is a valid day of the given month and year
+Date = tuple[int, int, int]
+YEAR = 0
+MONTH = 1
+DAY = 2
+
+# column numbers of data within input csv file
+INPUT_SID = 0
+INPUT_TITLE = 2
+INPUT_CAST = 4
+INPUT_DATE = 6
+INPUT_CATEGORIES = 10
+
+
+def read_file(filename: str) -> (dict[str, Date],
+                                 dict[str, list[str]],
+                                 dict[str, list[str]],
+                                 dict[str, str]):
+    """
+    Populates and returns a tuple with the following 4 dictionaries
+    with data from valid filename.
+    
+    4 dictionaries returned as a tuple:
+    - dict[show id: date added to Netflix]
+    - dict[show id: list of unique actor names]
+    - dict[category: list of unique show ids]
+    - dict[show id: show title]
+
+    Keys without a corresponding value are not added to the dictionary.
+    For example, the show 'First and Last' in the input file has no cast,
+    therefore an entry for 'First and Last' is not added 
+    to the dictionary dict[show id: list of unique actor names]
+
+    The list of actors for each key in
+      dict[show id: list of unique actor names]
+      should be in the order they appear on the line in the input file.
+      If the line has duplicated actor names, the unique actor name 
+      is added once for the first time it occurs in the line.
+    
+    Precondition: file is csv with data in expected columns 
+        and contains a header row with column titles
+        Show ids within the file are unique.
+        
+    >>> read_file('0lines_data.csv')
+    ({}, {}, {}, {})
+    
+    >>> read_file('11lines_data.csv')  # doctest: +NORMALIZE_WHITESPACE
+    ({'81217749': (2019, 11, 15),
+      '70303496': (2018, 9, 6),
+      '70142798': (2018, 9, 5),
+      '80999063': (2018, 10, 5),
+      '80190843': (2018, 9, 7),
+      '80119349': (2017, 9, 29),
+      '70062814': (2018, 9, 5),
+      '80182115': (2017, 9, 29),
+      '80187722': (2018, 10, 12),
+      '70213237': (2018, 10, 2),
+      '70121522': (2019, 8, 1)},
+     {'81217749': ['Naseeruddin Shah'],
+      '70303496': ['Aamir Khan',
+                   'Anuskha Sharma',
+                   'Sanjay Dutt',
+                   'Saurabh Shukla',
+                   'Parikshat Sahni',
+                   'Sushant Singh Rajput',
+                   'Boman Irani',
+                   'Rukhsar'],
+      '70142798': ['Jirayu La-ongmanee',
+                   'Charlie Trairat',
+                   'Worrawech Danuwong',
+                   'Marsha Wattanapanich',
+                   'Nicole Theriault',
+                   'Chumphorn Thepphithak',
+                   'Gacha Plienwithi',
+                   'Suteerush Channukool',
+                   'Peeratchai Roompol',
+                   'Nattapong Chartpong'],
+      '80999063': ['Elyse Maloway',
+                   'Vincent Tong',
+                   'Erin Matthews',
+                   'Andrea Libman',
+                   'Alessandro Juliani',
+                   'Nicole Anthony',
+                   'Diana Kaarina',
+                   'Ian James Corlett',
+                   'Britt McKillip',
+                   'Kathleen Barr'],
+      '70062814': ['Ananda Everingham',
+                   'Natthaweeranuch Thongmee',
+                   'Achita Sikamana',
+                   'Unnop Chanpaibool',
+                   'Titikarn Tongprasearth',
+                   'Sivagorn Muttamara',
+                   'Chachchaya Chalemphol',
+                   'Kachormsak Naruepatr'],
+      '80187722': ['Frank Grillo'],
+      '70213237': ['Graham Chapman',
+                   'Eric Idle',
+                   'John Cleese',
+                   'Michael Palin',
+                   'Terry Gilliam',
+                   'Terry Jones'],
+      '70121522': ['Aamir Khan',
+                   'Kareena Kapoor',
+                   'Madhavan',
+                   'Sharman Joshi',
+                   'Omi Vaidya',
+                   'Boman Irani',
+                   'Mona Singh',
+                   'Javed Jaffrey']},
+     {'Documentaries': ['81217749', '80119349', '80182115'],
+      'International Movies': ['81217749',
+                               '70303496',
+                               '70142798',
+                               '80119349',
+                               '70062814',
+                               '70121522'],
+      'Comedies': ['70303496', '70121522'],
+      'Dramas': ['70303496', '70121522'],
+      'Horror Movies': ['70142798', '70062814'],
+      'Children & Family Movies': ['80999063'],
+      'Docuseries': ['80190843', '80187722', '70213237'],
+      'British TV Shows': ['70213237']},
+     {'81217749': 'SunGanges',
+      '70303496': 'PK',
+      '70142798': 'Phobia 2',
+      '80999063': 'Super Monsters Save Halloween',
+      '80190843': 'First and Last',
+      '80119349': 'Out of Thin Air',
+      '70062814': 'Shutter',
+      '80182115': 'Long Shot',
+      '80187722': 'FIGHTWORLD',
+      '70213237': "Monty Python's Almost the Truth",
+      '70121522': '3 Idiots'})
+    """
+    # TODO: complete this function according to the documentation
+    # Important: DO NOT delete the header row from the csv file,
+    # your function should read the header line and ignore it (do nothing with it)
+    # All files we test your function with will have this header row!
+
+
+def query(filename: str, category: str, date: Date, actors: list[str]
+          ) -> list[tuple[str, str]]:
+    """
+    returns a list of sorted tuples containing (show title, show id) pairs 
+    of only those shows that:
+    - are of the given category
+    - have at least one of the actor names in actors in the cast
+      If no actors are specified (empty list), all actors in library are valid;
+    - were added to Netflix before the given date
+    
+    Precondition: category and actor names must match case exactly. 
+    For example:
+    'Comedies' doesn't match 'comedies', 'Aamir Khan' doesn't match 'aamir khan'
+    
+    You MUST call read_file and use look ups in the returned dictionaries 
+    to help solve this problem in order to receive marks.
+    You can and should design additional helper functions to solve this problem.
+    
+    >>> query('0lines_data.csv', 'Comedies', (2019, 9, 5), ['Aamir Khan'])
+    []
+    
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), [])
+    [('3 Idiots', '70121522'), ('PK', '70303496')]
+
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), ['Aamir Khan'])
+    [('3 Idiots', '70121522'), ('PK', '70303496')]
+        
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), ['Sanjay Dutt'])
+    [('PK', '70303496')]
+    
+    >>> query('11lines_data.csv', 'International Movies', (2019, 9, 5), \
+    ['Aamir Khan', 'Mona Singh', 'Achita Sikamana'])
+    [('3 Idiots', '70121522'), ('PK', '70303496'), ('Shutter', '70062814')]
+    
+    >>> query('11lines_data.csv', 'International Movies', (2019, 8, 1), \
+              ['Aamir Khan', 'Mona Singh', 'Achita Sikamana'])
+    [('PK', '70303496'), ('Shutter', '70062814')]
+    
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), \
+              ['not found', 'not found either'])
+    []
+    
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), \
+              ['Aamir Khan', 'not found', 'not found either']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('PK', '70303496')]
+    
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), \
+              ['not found', 'Aamir Khan', 'not found either']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('PK', '70303496')]
+    
+    >>> query('11lines_data.csv', 'Comedies', (2019, 9, 5), \
+              ['not found', 'not found either', 'Aamir Khan']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('PK', '70303496')]
+    
+    >>> query('large_data.csv', 'Comedies', (2019, 9, 5), \
+              ['Aamir Khan', 'Mona Singh', 'Achita Sikamana']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('Andaz Apna Apna', '20258084'),
+     ('PK', '70303496')]
+    
+    >>> query('large_data.csv', 'Comedies', (2020, 9, 5), \
+              ['Aamir Khan', 'Mona Singh', 'Achita Sikamana']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('Andaz Apna Apna', '20258084'),
+     ('Dil Chahta Hai', '60021525'), ('Dil Dhadakne Do', '80057585'),
+     ('PK', '70303496'), ('Zed Plus', '81213884')]
+    
+    >>> query('large_data.csv', 'International Movies', (2020, 9, 5), \
+              ['Aamir Khan', 'Mona Singh', 'Achita Sikamana']\
+             ) # doctest: +NORMALIZE_WHITESPACE
+    [('3 Idiots', '70121522'), ('Andaz Apna Apna', '20258084'), 
+     ('Dangal', '80166185'), ('Dhobi Ghat (Mumbai Diaries)', '70144331'),
+     ('Dil Chahta Hai', '60021525'), ('Dil Dhadakne Do', '80057585'), 
+     ('Lagaan', '60020906'), ('Madness in the Desert', '80229953'),
+     ('PK', '70303496'), ('Raja Hindustani', '17457962'), 
+     ('Rang De Basanti', '70047320'), ('Secret Superstar', '80245408'), 
+     ('Shutter', '70062814'), ('Taare Zameen Par', '70087087'),
+     ('Talaash', '70262614'), ('Zed Plus', '81213884')]
+    """
+    # TODO: complete this function according to the documentation
